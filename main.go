60dd93d060223e555390e2eca6ff391a33e0377d
Calvin Akoonyatse <cakoonyatse@gmail.com>
2024-01-12 12:29:27 +0200

    Update main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"jwt-auth/database"
+	"jwt-auth/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
+
+	app.Listen(":3000")
+}
