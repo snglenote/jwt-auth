6361f7f2b26eaf9c4cb4c1218777bd9b3734b763
Calvin Akoonyatse <cakoonyatse@gmail.com>
2024-01-12 12:30:03 +0200

    Create auth.go

---
+++ database/connection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"jwt-auth/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("test:snglenote@/jwt-auth"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
